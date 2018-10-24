cd4119b5f1e265fed9c629548d2b30a050642eff
Kuba Bartwicki <kuba.bartwicki@digital.cabinet-office.gov.uk>
2018-10-24 12:45:14 +0100

    Start page ish

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Run a service community',
 
   // Default port that prototype runs on
   port: '3000',
